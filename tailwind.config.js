0d532fb785693fa3e2fcad3093f88f6dd517733f
Milena Hochheim <104088089+milenahochheim@users.noreply.github.com>
2022-09-27 20:47:06 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,vue}"],
+  body: {
+    background: "#e3e1ff",
+    color: "#444",
+  },
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
